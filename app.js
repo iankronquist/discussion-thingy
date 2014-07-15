90c5ce8e98a8711a17287133dcb95e0c3f99651b
Ian Kronquist <iankronquist@teleport.com>
2014-07-14 20:00:32 -0700

    Remove old comments
    We don't need to comment out old code in version control.

--- app.js
+++ app.js
@@ -5,50 +5,28 @@
 
 var express = require('express');
 
-//var http = require('http');
 var path = require('path');
 
 var app = module.exports = express()
 var server = require('http').createServer(app)
 var io = require('./sockets').listen(server) //this is where sockets.js comes in
-//var _ = require('lodash');
 
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
-/*
-
-var uristring = 
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://localhost/test';
-
-
-var mongoose = require('mongoose');
-mongoose.connect(uristring);
-
-var db = mongoose.connection;
-*/
-
 var points = require('./points')
 
 
-
-
 // all environments
-
-
 app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
