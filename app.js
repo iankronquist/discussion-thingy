c2362ca890e72fa588727f242d1169c873fe9b57
Ian Kronquist <iankronquist@teleport.com>
2014-07-18 00:13:52 -0700

    Add sequelize db and config file

--- app.js
+++ app.js
@@ -3,33 +3,53 @@
  * Module dependencies.
  */
 
+//Import configuration
+var config = require("./config.json");
+
+//Set up database based on config
+var Sequelize = require('sequelize')
+    , sequelize = new Sequelize(config.db.name, config.db.username,
+	config.db.password, {
+		dialect: config.db.dialect,
+		port: config.db.port,
+});
+
+
+sequelize
+	.authenticate()
+	.complete(function(err) {
+		if (!!err) {
+			console.log('Unable to connect to the database:', err);
+			throw "DB connection error!";
+		} else {
+			console.log('Connection has been established successfully.');
+	}
+});
+
+exports.sequelize = sequelize;
+
 var express = require('express');
 
 var path = require('path');
+
+
 
 var app = module.exports = express()
 var server = require('http').createServer(app)
 var io = require('./sockets').listen(server) //this is where sockets.js comes in
 
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://localhost:7474');
-
 var points = require('./points')
 
 // all environments
-
-
 app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -58,5 +78,4 @@ app.get('/requestTopics', function(req, res){
 app.post('/submit', function(req, res, data){
 	points.new_point(req.body, function(){res.send('')});
 })
-
 
