2828bdd038e1233689b07b7b2790712107fdea19
Ian Kronquist <iankronquist@teleport.com>
2014-07-17 22:30:31 -0700

    Update vagrant environment and package.json

--- etc/vagrant_provision.sh
+++ etc/vagrant_provision.sh
@@ -1,7 +1,6 @@
 apt-get update
 
-apt-get install -y -q npm git openjdk-7-jre-headless neo4j
+apt-get install -y -q git nodejs npm
 ln -sf /usr/bin/nodejs /usr/local/bin/node
 export LC_ALL="en_US.UTF-8"
 sudo locale-gen en_US.UTF-8
-echo "provision finished"
