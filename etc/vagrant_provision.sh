28bc4f1a90d327334e0c2677afe465b098dc8410
Ian Kronquist <iankronquist@teleport.com>
2014-07-17 21:59:59 -0700

    Fix ubuntu vm locale complaint

--- etc/vagrant_provision.sh
+++ etc/vagrant_provision.sh
@@ -2,4 +2,6 @@ apt-get update
 
 apt-get install -y -q npm git openjdk-7-jre-headless neo4j
 ln -sf /usr/bin/nodejs /usr/local/bin/node
+export LC_ALL="en_US.UTF-8"
+sudo locale-gen en_US.UTF-8
 echo "provision finished"
